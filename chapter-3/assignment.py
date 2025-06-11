6ccb6366b3d3825732912dcc38d3cfac2cbd3ebb
Jahid Sheikh <zjahid19@gmail.com>
2025-06-11 12:22:54 +0000

    added assignment file

---
+++ chapter-3/assignment.py
@@ -0,0 +1,18 @@
+# Assign value to number_1 variable
+number_1 = 100
+
+# Add 10 to number_1 variable and assign back to number_1 (Addition Assignment)
+number_1 += 10
+print('Addition assignment result:', number_1)
+
+# Subtract 10 from number_1 variable and assign back to number_1 (Subtraction Assignment)
+number_1 -= 10
+print('Subtraction assignment result:', number_1)
+
+# Multiply number_1 variable by 10 and assign back to number_1 (Multiplication Assignment)
+number_1 *= 10
+print('Multiplication assignment result:', number_1)
+
+# Divide number_1 variable by 10 and assign back to number_1 (Division Assignment)
+number_1 /= 10
+print('Division assignment result:', number_1)
