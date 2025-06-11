209eb57c6d471fd3b63d672c37a539f0124b1ace
Jahid Sheikh <zjahid19@gmail.com>
2025-06-11 12:14:09 +0000

    added operator examples

---
+++ chapter-3/arithmatic.py
@@ -0,0 +1,30 @@
+number_1 = 50
+number_2 = 20 
+
+# Addition of two numbers
+addition_result = number_1 + number_2
+print('The addition of two numbers is:', addition_result)
+
+# Subtraction of two numbers
+subtraction_result = number_1 - number_2
+print('The subtraction of two numbers is:', subtraction_result)
+
+# Multiplication of two numbers
+multiplication_result = number_1 * number_2
+print('The multiplication of two numbers is:', multiplication_result)
+
+# Division of two numbers
+division_result = number_1 / number_2
+print('The division of two numbers is:', division_result)
+
+# Floor Division of two numbers
+floor_division_result = number_1 // number_2
+print('The floor division of two numbers is:', floor_division_result)
+
+# Modulus of two numbers
+modulus_result = number_1 % number_2
+print('The modulus of two numbers is:', modulus_result)
+
+# Exponentiation of two numbers
+exponentiation_result = number_1 ** number_2
+print('The exponentiation of two numbers is:', exponentiation_result)
---
+++ chapter-3/comparison.py
@@ -0,0 +1,26 @@
+number_1 = 50  
+number_2 = 100  
+
+# Equal to operator ==  
+equal_result = (number_1 == number_2)  
+print('Equal to result is:', equal_result)  
+
+# Not equal to operator !=  
+not_equal_result = (number_1 != number_2)  
+print('Not equal to result is:', not_equal_result)  
+
+# Less than operator <  
+less_than_result = (number_1 < number_2)  
+print('Less than result is:', less_than_result)  
+
+# Greater than operator >  
+greater_than_result = (number_1 > number_2)  
+print('Greater than result is:', greater_than_result)  
+
+# Less than or equal to operator <=  
+less_than_equal_result = (number_1 <= number_2)  
+print('Less than or equal to result is:', less_than_equal_result)  
+
+# Greater than or equal to operator >=  
+greater_than_equal_result = (number_1 >= number_2)  
+print('Greater than or equal to result is:', greater_than_equal_result)  
